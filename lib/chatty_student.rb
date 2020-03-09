2b69363203439afe2c47c60cee3cac29c966ba04
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-09 23:21:23 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,1 +1,12 @@
 class ChattyStudent < Student
+  
+  def hello
+    super
+    "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didnt't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end
+  
+  def raise_hand
+    super.times(10)
+  end  
+  
+end
