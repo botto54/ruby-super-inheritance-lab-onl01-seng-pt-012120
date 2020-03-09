76634a42739b7c86d7b9da67af8ad2eef612fa0e
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-09 23:35:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
-class Student
+# class Student
   
-  def hello
-    "Hey there! I'm so excited to learn stuff."
-  end
+#   def hello
+#     "Hey there! I'm so excited to learn stuff."
+#   end
   
-  def raise_hand
-    "Pick me!"
-  end
+#   def raise_hand
+#     "Pick me!"
+#   end
   
-end
+# end
